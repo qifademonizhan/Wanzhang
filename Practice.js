ad367151e8aa3819ca76e1373cbade123eb08011
qifademonizhan <kimhs21059@yn36eng.com>
2025-11-07 11:46:08 +0900

    Create Practice.js

---
+++ Practice.js
@@ -0,0 +1,11 @@
+const sans = document.getElementById('newSans')
+sans.addEventListener
+
+function changePictureInitially() {
+  danceCount = danceCount + 1
+  document.getElementById().src =  `dancingSans${danceCount}`
+  setTimeout(() => { 
+    document.getElementById('dancing').id = 'newSans'}, 500 );
+  
+  
+}
